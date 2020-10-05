eac01620153021781713306b6c55957243f8f8cc
ArthurVigneault <arthur.vigneault@laposte.net>
2020-10-05 18:39:51 +0200

    6 done
    ds

--- src/levels/level_container.gd
+++ src/levels/level_container.gd
@@ -1,9 +1,9 @@
-﻿extends Node2D
+extends Node2D
 
 var current_level_number = 1
 
 
-onready var current_level_reference = $Test_lvl_5
+onready var current_level_reference = $Test_lvl_6
 
 func _ready():
 	$Camera2D.init(current_level_reference.get_node("Spaceship")) 
