2163bc9f15cd90261210f509f57a5b0bfb26c567
ArthurVigneault <arthur.vigneault@laposte.net>
2020-10-05 13:46:15 +0200

    skusku
    sku

--- src/levels/level_container.gd
+++ src/levels/level_container.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var current_level_number = 1
-onready var current_level_reference = $Test_lvl_2
+onready var current_level_reference = $Test_lvl_4
 
 func _ready():
 	$Camera2D.init(current_level_reference.get_node("Spaceship")) 
